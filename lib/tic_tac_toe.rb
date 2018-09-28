f3f9e997f635e19fb1c6464fc123d13f578f01dc
Ray Valdez <ray_valdez@hotmail.co.uk>
2018-09-28 15:32:19 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -68,3 +68,22 @@ def current_player(board)
     return "O"
   end
 end
+
+ef won?(board)
+  WIN_COMBINATIONS.each {|combination|
+    index_0 = combination[0]
+    index_1 = combination[1]
+    index_2 = combination[2]
+
+    position_1 = board[index_0]
+    position_2 = board[index_1]
+    position_3 = board[index_2]
+
+    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+      return combination
+    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+      return combination
+    end
+  }
+  return false
+end

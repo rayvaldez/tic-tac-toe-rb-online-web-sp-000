1c43951c3afb76103ebefecd07bf6fd890ca9bbc
Ray Valdez <ray_valdez@hotmail.co.uk>
2018-09-28 14:32:34 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -41,7 +41,7 @@ def turn(board)
   index = input_to_index(user_input)
   if valid_move?(board, index)
     player_token = current_player(board)
-    move(board, index, player_token)
+    make_move(board, index, player_token)
     display_board(board)
   else
     turn(board)

edb76d7629ccf1f3d876f9968e58bdd679338404
Ray Valdez <ray_valdez@hotmail.co.uk>
2018-09-28 14:36:04 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -36,17 +36,17 @@ def valid_move?(board, index)
 end
 
 def turn(board)
-  puts "Please choose a number 1-9:"
-  user_input = gets.chomp
-  index = input_to_index(user_input)
-  if valid_move?(board, index)
-    player_token = current_player(board)
-    make_move(board, index, player_token)
+  puts "Please enter 1-9:"
+  input = gets.chomp
+  index = input_to_index(input)
+  if valid_move?(board, index) == true
+    move(board, index)
     display_board(board)
   else
     turn(board)
   end
 end
+
 
 def turn_count(board)
   turn = 0

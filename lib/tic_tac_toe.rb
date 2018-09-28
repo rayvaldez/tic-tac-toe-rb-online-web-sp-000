56908adf463041446d6f5df7def34606337ac88c
Ray Valdez <ray_valdez@hotmail.co.uk>
2018-09-28 15:30:28 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -51,6 +51,16 @@ def turn(board)
   end
 end
 
+def turn_count(board)
+  counter = 0
+  board.each do |turn|
+    if turn == "X" || turn == "O"
+      counter += 1
+    end
+  end
+    return counter
+end
+
 def current_player(board)
   if turn_count(board) % 2 == 0
     return "X"

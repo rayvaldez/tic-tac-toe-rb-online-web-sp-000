0e8db3d6fdd7f6313568150f14d2003d1f197ec1
Ray Valdez <ray_valdez@hotmail.co.uk>
2018-09-28 15:39:01 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -126,4 +126,12 @@ def play(board)
   until over?(board) == true
     turn(board)
   end
+  
+  if won?(board)
+    puts "Congratulations #{winner(board)}!"
+  elsif draw?(board)
+    puts "Cats Game!"
+  end
 end
+
+

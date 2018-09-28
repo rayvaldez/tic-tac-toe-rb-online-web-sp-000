74b460419b037e99338708e18828b968cb35060c
Ray Valdez <ray_valdez@hotmail.co.uk>
2018-09-28 14:38:52 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,16 +1,5 @@
 require 'pry'
 
-WIN_COMBINATIONS = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [6, 4, 2]
-]
-
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -23,16 +12,20 @@ def input_to_index(user_input)
   user_input.to_i - 1
 end
 
-def make_move(board, index, current_player)
-  board[index] = current_player
-end
-
 def position_taken?(board, index)
-  !(board[index].nil? || board[index] == " ")
+  if board[index] == " " || board[index] == "" || board[index] ==nil
+    return false
+  else
+    return true
+  end
 end
 
 def valid_move?(board, index)
   index.between?(0,8) && !position_taken?(board, index)
+end
+
+def move(board, index, character = "X")
+  board[index] = character
 end
 
 def turn(board)
@@ -40,7 +33,7 @@ def turn(board)
   input = gets.chomp
   index = input_to_index(input)
   if valid_move?(board, index) == true
-    make_move(board, index, current_player)
+    move(board, index)
     display_board(board)
   else
     turn(board)
